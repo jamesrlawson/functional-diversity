b5cf049f02394e20e76534c33dc4ff06f1f98af7
James Lawson <james.lawson@mq.edu.au>
2014-06-25 12:56:06 +1000

    first commit, FD project works with LHS, woody and flowering period traits

---
+++ scripts/analysis.R
@@ -0,0 +1,152 @@
+#############################################################################################
+#################### ANALYSIS FOR ONLY SPP. WITH COMPLETE DATA ####################
+#############################################################################################
+#################### LHS + woody/herbaceous ##################################################
+
+source("scripts/functions.R")
+options(stringsAsFactors = FALSE)
+library(plyr)
+library(ggplot2)
+library(reshape)
+library(FD)
+
+#############################################################################################
+############################# GET RELATIVE ABUNDANCES FOR ALL TRAITS ########################
+#############################################################################################
+
+# load in data
+
+alltraits <- read.csv("data/alltraits.csv", header=TRUE)
+percentcover <- read.csv("data/percentcover.csv", header=TRUE)
+hydro <- read.csv("data/hydronorm.csv", header=TRUE)
+
+# remove observations that average less than 1% cover
+percentcover <- subset(percentcover, avgcover > 1)
+
+# generate df with total % cover (across all strata) of species at each plot
+plotsums <- ddply(percentcover, .(plotID, species), summarise, speciescover = sum(avgcover))
+
+# remove WD and get df with only species that have complete data for all traits
+
+allspp <- alltraits
+allspp <- na.omit(allspp)
+
+allspp.cover <- findtraitvals(plotsums, allspp)
+allspp.totalcover <- ddply(allspp.cover, .(plotID), summarise, totalcover = sum(speciescover))
+allspp.cover <- merge(allspp.cover, allspp.totalcover, by.x = "plotID", by.y = "plotID")
+allspp.cover <-relabund(allspp.cover)
+
+test <- ddply(allspp.cover, .(plotID), summarise, summedRelCover = (sum(relcover)))
+print(test)
+rm(test)
+
+#######
+
+traits <- allspp.cover[2]
+traits <- cbind(traits, allspp.cover[4:8])
+traits <- ddply(traits, .(species), unique)
+#traits$growthform <- as.factor(traits$growthform)
+
+# transform traits
+
+hist(log2(traits$seedmass))
+hist(log(traits$SLA))
+hist(log10(traits$maxheight), breaks = 5)
+hist((traits$flowering.period)) # non-normal distribution with any common transform
+
+traits$seedmass <- log2(traits$seedmass)
+traits$SLA <- log(traits$SLA)
+traits$maxheight <- log10(traits$maxheight)
+
+traits$woody <- as.numeric(traits$woody)
+
+# create abun, in correct input format for FD analysis
+
+abun <- data.frame(allspp.cover[1])
+abun <- cbind(abun, allspp.cover[2])
+abun <- cbind(abun, allspp.cover[11])
+abun <- cast(abun, plotID ~ species, value="relcover", fill=0)
+rownames(abun) <- abun$plotID
+abun$plotID <- NULL
+
+spp <- traits$species 
+traits$species <- NULL
+rownames(traits) <- spp
+rm(spp)
+
+### run FD analysis ### important that traits are scaled (stand.x = TRUE)
+### 6 or 12 groups ###
+
+FD.dbfd <- dbFD(traits, 
+                abun, 
+                w.abun = TRUE, 
+                stand.x = TRUE,
+                ord = c("podani"),
+                corr = c("cailliez"),
+                calc.FGR = TRUE, 
+                calc.FDiv = TRUE, 
+                calc.CWM=TRUE, 
+                clust.type="ward", 
+                print.pco=TRUE, 
+#                scale.RaoQ=TRUE, 
+                stand.FRic=TRUE)
+
+hydroplots <- hydro
+
+hydroplots$FDis <- FD.dbfd$FDis
+hydroplots$FDiv <- FD.dbfd$FDiv
+hydroplots$FRic <- FD.dbfd$FRic
+hydroplots$FEve <- FD.dbfd$FEve
+hydroplots$RaoQ <- FD.dbfd$RaoQ
+hydroplots$FGR <- FD.dbfd$FGR
+hydroplots$nbsp <- FD.dbfd$nbsp
+
+CWM <- FD.dbfd$CWM
+
+hydroplots$SLA.CWM <- CWM$SLA
+hydroplots$seedmass.CWM <- CWM$seedmass
+hydroplots$maxheight.CWM <- CWM$maxheight
+hydroplots$flowering.period.CWM <- CWM$flowering.period
+
+## try a Tukey's test to compare hydro categories ##
+
+FDis.aov <- aov(hydroplots$FDis ~ as.factor(hydroplots$category))
+TukeyHSD(FDis.aov)
+
+## plot everything against hydro ##
+
+plot.linear(hydroplots, hydroplots$FDis, FD)
+plot.linear(hydroplots, hydroplots$FDiv, FD)
+plot.linear(hydroplots,hydroplots$FRic, FD)
+plot.linear(hydroplots, hydroplots$FEve, FD)
+plot.linear(hydroplots, hydroplots$nbsp, FD)
+
+plot.linear(hydroplots, hydroplots$SLA.CWM, CWM)
+plot.linear(hydroplots,hydroplots$seedmass.CWM, CWM)
+plot.linear(hydroplots, hydroplots$maxheight.CWM, CWM)
+plot.linear(hydroplots, hydroplots$flowering.period.CWM, CWM)
+
+plot.quad(hydroplots, hydroplots$FDis, FD)
+plot.quad(hydroplots, hydroplots$FDiv, FD)
+plot.quad(hydroplots,hydroplots$FRic, FD)
+plot.quad(hydroplots, hydroplots$FEve, FD)
+plot.quad(hydroplots, hydroplots$nbsp, FD)
+
+plot.quad(hydroplots, hydroplots$SLA.CWM, CWM)
+plot.quad(hydroplots,hydroplots$seedmass.CWM, CWM)
+plot.quad(hydroplots, hydroplots$maxheight.CWM, CWM)
+plot.quad(hydroplots, hydroplots$flowering.period.CWM, CWM)
+
+
+getStats(hydroplots, hydroplots$FDis, FD)
+getStats(hydroplots, hydroplots$FDiv, FD)
+getStats(hydroplots, hydroplots$FRic, FD)
+getStats(hydroplots, hydroplots$FEve, FD)
+getStats(hydroplots, hydroplots$nbsp, FD)
+
+getStats(hydroplots, hydroplots$SLA.CWM, CWM)
+getStats(hydroplots,hydroplots$seedmass.CWM, CWM)
+getStats(hydroplots, hydroplots$maxheight.CWM, CWM)
+getStats(hydroplots, hydroplots$flowering.period.CWM, CWM)
+
+
---
+++ scripts/functions.R
@@ -0,0 +1,214 @@
+library(plyr)
+library(ggplot2)
+
+# find species in % cover list associated with trait values
+
+findtraitvals <- function(plotsums, trait) {
+  
+  
+  
+  y <- data.frame()
+  
+  for(i in 1:nrow(plotsums)) {
+    
+    species <- as.data.frame(plotsums$species)
+    colnames(species) <- c("species")
+    x <- species[i,]
+    
+    # grep returns index of matches (similar to match but returns all indexes, not just the first)
+    
+    x <- grep(x, plotsums$species, fixed=TRUE) 
+    x <- plotsums[x,]
+    x <- merge(x, trait, by.x = "species", by.y = "species")
+    
+    # iteratively add things to dataframe y!
+    y <- rbind(y,x)
+    
+  }
+  # subset by matches 
+  #x <- plotsums[na.omit(x), ] 
+  
+    y <- ddply(y, .(plotID), unique)
+    return(y)
+}
+
+
+# relabund finds relative % cover for each species at each site
+
+relabund <- function(df) {
+                              
+                relcover <- df$speciescover / df$totalcover 
+                cbind(df, "relcover" = relcover)
+}
+  
+
+spread <- function(x) (diff(range(x)))
+
+
+plot.linear <- function(df, var, trait) { # var is alphaT/betaT/ts/Rs, etc.
+  
+  figureDir <- "C:/Users/JLawson/Desktop/stuff/data/analysis/R/functional diversity/output/figures"
+  traitDir <- deparse(substitute(trait))
+  varDir <- deparse(substitute(var))
+  
+  outDir <- sprintf("%s/%s/%s/%s", figureDir, traitDir, varDir, "linear")
+  
+  dir.create(outDir, recursive=TRUE)
+    
+  for(i in 1:ncol(df)) {
+    hydro <- df[[i]]  
+    hydroname <- as.expression(colnames(df[i]))   
+    fit.linear <- lm(var ~ hydro, data = df)
+    
+  #  padj <- labels$p.adj[i]
+    r2 <- signif(summary(fit.linear)$r.squared, 5)
+    pval <- anova(fit.linear)[1,"Pr(>F)"]
+  
+  labels <- list("ylab" <- c(deparse(substitute(trait))),
+                 "catname" <- as.factor(df$category)
+  )
+    
+    png(sprintf("%s/%s_pval-%s_r2-%s.png", outDir, hydroname, pval, r2), width = 600, height = 500)
+    
+    p <- qplot(hydro, var, data = df) 
+    p <- p + geom_point()
+    p <- p + geom_point(aes(shape = labels[2]), size =3)
+    p <- p + scale_shape_discrete(name = "Hydrological \n class", labels = c("stable winter baseflow", "unpredictable baseflow", "unpredictable intermittent"))
+    p <- p + stat_smooth(aes(group = 1), method = "lm", formula = y ~ x, se=TRUE, col="black") 
+    p <- p + xlab(hydroname)
+  #  p <- p + ylim(0.45, 0.75)
+    p <- p + ylab(labels$ylab)
+   # p <- p + annotate("text",                    
+   #                   x=max(hydro)/1.5, y=0.5,
+   #                   label=paste("R^2 = ",signif(summary(fit.quad)$r.squared, 5),
+   #                               "\np.adj =",labels$p.adj[i]),
+   #                   size = 4)
+    #    p <- p + ggtitle(labels$title)    
+    p <- p + theme_minimal() # if you want to use a preset theme and then modify it, call it first
+    p <- p + theme(panel.grid.major = element_blank(),
+                   panel.grid.minor = element_blank(),
+                   axis.line = element_line(size=.7, color = "black"),
+                   legend.position = "bottom",
+                   panel.background = element_blank(),      
+                   plot.title = element_text(size=12),
+                   axis.text = element_text(size=12),
+                   text = element_text(size=12))   
+    
+    print(p)
+    dev.off()
+  }
+}
+
+
+plot.quad <- function(df, var, trait, labels) { # var is alphaT/betaT/ts/Rs, etc.
+  
+  
+  figureDir <- "C:/Users/JLawson/Desktop/stuff/data/analysis/R/functional diversity/output/figures"
+  traitDir <- deparse(substitute(trait))
+  varDir <- deparse(substitute(var))
+  
+  outDir <- sprintf("%s/%s/%s/quad", figureDir, traitDir, varDir)
+  
+  dir.create(outDir, recursive=TRUE)
+  
+  labels <- list("ylab" <- c(deparse(substitute(trait))),
+                 "catname" <- as.factor(df$category)
+  )
+  
+  for(i in 1:ncol(df)) {
+    hydro <- df[[i]]  
+    hydroname <- as.expression(colnames(df[i]))   
+    fit.quad <- lm(var ~ hydro + I(hydro^2), data = df)
+    
+    #  padj <- labels$p.adj[i]
+    r2 <- signif(summary(fit.quad)$r.squared, 5)
+    pval <- anova(fit.quad)[1,"Pr(>F)"]
+    
+    png(sprintf("%s/%s_pval-%s_r2-%s.png", outDir, hydroname, pval, r2), width = 600, height = 500)
+    
+    p <- qplot(hydro, var, data = df) 
+    p <- p + geom_point()
+    p <- p + geom_point(aes(shape = labels$catname), size =3)
+    p <- p + scale_shape_discrete(name = "Hydrological \n class", labels = c("stable winter baseflow", "unpredictable baseflow", "unpredictable intermittent"))
+    p <- p + stat_smooth(aes(group = 1), method = "lm", formula = y ~ x + I(x^2), se=TRUE, col="black") 
+    p <- p + xlab(hydroname)
+    #  p <- p + ylim(0.45, 0.75)
+    p <- p + ylab(labels$ylab)
+    # p <- p + annotate("text",                    
+    #                   x=max(hydro)/1.5, y=0.5,
+    #                   label=paste("R^2 = ",signif(summary(fit.quad)$r.squared, 5),
+    #                               "\np.adj =",labels$p.adj[i]),
+    #                   size = 4)
+    #    p <- p + ggtitle(labels$title)    
+    p <- p + theme_minimal() # if you want to use a preset theme and then modify it, call it first
+    p <- p + theme(panel.grid.major = element_blank(),
+                   panel.grid.minor = element_blank(),
+                   axis.line = element_line(size=.7, color = "black"),
+                   legend.position = "bottom",
+                   panel.background = element_blank(),      
+                   plot.title = element_text(size=12),
+                   axis.text = element_text(size=12),
+                   text = element_text(size=12))   
+    
+    print(p)
+    dev.off()
+  }
+}
+
+# keeping this for future reference. returns the dataframe D_Sna_hydro_*trait*
+
+gettrait<- function(trait) {
+  
+  traitname <- deparse(substitute(trait))
+  get(sprintf("D_Sna_hydro_%s", traitname))
+  
+}
+
+
+getStats <- function(df, var, trait) {
+  
+  # create / set output directory
+  
+  statsDir <- "C:/Users/JLawson/Desktop/stuff/data/analysis/R/functional diversity/output/stats"
+   
+  dir.create(statsDir, recursive=TRUE, showWarnings=FALSE)
+
+  # output stats for each metric to dataframe
+
+  y <- data.frame()
+  
+  for(i in 1:ncol(df)) {
+    
+    hydro <- df[[i]]  
+    hydroname <- as.expression(colnames(df[i]))  
+    
+    fit.linear <- lm(var ~ hydro, data = df)
+    fit.quad <- lm(var ~ hydro + I(hydro^2), data = df)
+    
+    r2.linear <- signif(summary(fit.linear)$r.squared, 5)
+    pval.linear <- anova(fit.linear)[1,"Pr(>F)"]
+    
+    r2.quad <- signif(summary(fit.quad)$r.squared, 5)
+    pval.quad <- anova(fit.quad)[1,"Pr(>F)"]
+    
+    x <- cbind(pval.linear, r2.linear, pval.quad, r2.quad)
+    
+    x <- as.data.frame(x)
+    
+    x <- cbind(as.character(hydroname), x)
+    
+    colnames(x) <- c("metric", "pval.linear", "r2.linear", "pval.quad", "r2.quad")
+    
+    if (pval.linear < 0.06) { 
+    y <- rbind(x,y)
+    }
+    
+  }
+  
+  var <- deparse(substitute(var))
+    
+  write.csv(y, sprintf("%s/%s_stats.csv", statsDir, var))
+   
+#  return(y)
+  
+}
